8c854a819b508ecd96f42e8294fe5b13ba3f2572
GuilhermeTGG <guilherme050592@gmail.com>
2022-11-05 06:41:03 -0300

    good

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,26 +3,26 @@ import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
                 <Link to="" className="fs-3 ubuntu navbar-brand">
                     Rick & Morty <span className="text-primary">WiKi</span>
                 </Link>
                 <button
-                    class="navbar-toggler"
+                    className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <NavLink to="" class="nav-link">Characters</NavLink>
-                        <NavLink to="/episodes" class="nav-link">Espisodes</NavLink>
-                        <NavLink to="/location" class="nav-link">Location</NavLink>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav fs-5">
+                        <NavLink to="" className="nav-link">Characters</NavLink>
+                        <NavLink to="/episodes" className="nav-link">Espisodes</NavLink>
+                        <NavLink to="/location" className="nav-link">Location</NavLink>
                     </div>
                 </div>
             </div>
