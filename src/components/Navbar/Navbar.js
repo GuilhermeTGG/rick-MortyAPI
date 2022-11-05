c2f666d1af4aa4743c89342880b6ec9d138e480c
GuilhermeTGG <guilherme050592@gmail.com>
2022-11-05 07:04:14 -0300

    NavbarOK

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import "../../App.css";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
             <div className="container">
                 <Link to="" className="fs-3 ubuntu navbar-brand">
                     Rick & Morty <span className="text-primary">WiKi</span>
@@ -16,11 +17,23 @@ const Navbar = () => {
                     aria-controls="navbarNavAltMarkup" aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span className="navbar-toggler-icon"></span>
+                    <style jsx>
+                        {`
+                        button[aria-expanded="false"] > .close {
+                            display : none;
+                        }
+
+                        button[aria-expanded="true"] > .open {
+                            display : none;
+                        }
+                        `}
+                    </style>
+                    <i class="fas fa-bars open fw-bold text-dark"></i>
+                    <i class="fas fa-times close fw-bold text-dark"></i>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav fs-5">
-                        <NavLink to="" className="nav-link">Characters</NavLink>
+                        <NavLink activeClassName="active" to="" className="nav-link">Characters</NavLink>
                         <NavLink to="/episodes" className="nav-link">Espisodes</NavLink>
                         <NavLink to="/location" className="nav-link">Location</NavLink>
                     </div>
@@ -31,3 +44,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
