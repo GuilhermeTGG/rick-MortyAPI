ffe126315097c3a06c388c3004de8100eba34505
GuilhermeTGG <guilherme050592@gmail.com>
2022-11-06 09:06:32 -0300

    navbarOK

--- src/Pages/Espisodes.js
+++ src/Pages/Espisodes.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Cards from '../components/Cards/Cards';
+import InputGroup from '../components/Filters/Category/InputGroup';
 
 const Espisodes = () => {
 
@@ -15,8 +16,8 @@ const Espisodes = () => {
             setInfo(data);
 
             let a = await Promise.all(
-                data.characters.map((x)=>{
-                    return fetch(x).then((res)=>res.json());
+                data.characters.map((x) => {
+                    return fetch(x).then((res) => res.json());
                 })
             );
             setResults(a);
@@ -32,11 +33,14 @@ const Espisodes = () => {
                     </span>
                 </h1>
                 <h5 className='text-center'>
-                    Air Date {air_date === "" ? "Unknown" : air_date}
+                    Air Date: {air_date === "" ? "Unknown" : air_date}
                 </h5>
             </div>
             <div className='row'>
-                <div className='col-3'>Pick Episodes</div>
+                <div className='col-3'>
+                <h4 className='text-center mb-4'>Pick Episodes</h4>
+                <InputGroup setID={setID} name="Episode" total={51} />
+                </div>
                 <div className='col-8'>
                     <div className='row'>
                         <Cards results={results} />
